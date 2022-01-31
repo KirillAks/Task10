e5b7e8f1c012bbaad0e66724899e7375ac2616c5
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2022-01-31 15:13:31 +0300

    Добавьте файлы проекта.

---
+++ Task10/Angle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool positive = true;
+
+        public int Gradus
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    positive = false;
+                }
+                else
+                {
+                    gradus = value % 360;
+                }                
+            }
+            get
+            {
+                return gradus;
+            }
+        }
+        public int Min
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    positive = false;
+                }
+                else
+                {
+                    Gradus += value / 60;
+                    min = value % 60;
+                }                
+            }
+            get
+            {
+                return min;
+            }
+        }
+        public int Sec
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    positive = false;
+                }
+                else
+                {
+                    Min += value / 60;
+                    sec = value % 60;
+                }               
+            }
+            get
+            {
+                return sec;
+            }
+        }
+        public Angle(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+
+        public double ToRadians()
+        {
+            return Math.PI * (gradus + (min / 60) + (sec / 3600)) / 180;
+        }
+    }
+}
---
+++ Task10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class Program
+    {
+        //Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд.
+        //Реализовать класс, в котором указанные значения представлены в виде свойств. Для свойств предусмотреть проверку корректности данных.
+        //Класс должен содержать конструктор для установки начальных значений, а также метод ToRadians для перевода угла в радианы.
+        //Создать объект на основе разработанного класса. Осуществить использование объекта в программе.
+
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                Console.WriteLine("Введите целочисленное значение градусов:");
+                int gradus = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите целочисленное значение минут:");
+                int min = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите целочисленное значение секунд:");
+                int sec = Convert.ToInt32(Console.ReadLine());
+                angle = new Angle(gradus, min, sec);
+            }while (angle.positive == false);
+            double angleRadians = angle.ToRadians();
+            Console.WriteLine("Угол в радианах равен {0}", angleRadians);
+            Console.ReadKey();
+        }
+    }
+}
